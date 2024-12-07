3ab35b34fb286f7fb9c0b1f1314ba085789e5063
Ryan Mahler <ryan.mahler@d2l.com>
2024-12-07 12:18:08 -0800

    Apply editorconfig
    Better than solving puzzles: applying consistent formatting.

--- 2024/2.py
+++ 2024/2.py
@@ -1,62 +1,62 @@
 input_data = [
-	"48 46 47 49 51 54 56",
-	"1 1 2 3 4 5",
-	"1 2 3 4 5 5",
-	"5 1 2 3 4 5",
-	"1 4 3 2 1",
-	"1 6 7 8 9",
-	"1 2 3 4 3",
-	"9 8 7 6 7",
-	"7 10 8 10 11",
-	"29 28 27 25 26 25 22 20",
-	"7 10 8 10 11",
-	"29 28 27 25 26 25 22 20",
-	"8 9 10 11"
+    "48 46 47 49 51 54 56",
+    "1 1 2 3 4 5",
+    "1 2 3 4 5 5",
+    "5 1 2 3 4 5",
+    "1 4 3 2 1",
+    "1 6 7 8 9",
+    "1 2 3 4 3",
+    "9 8 7 6 7",
+    "7 10 8 10 11",
+    "29 28 27 25 26 25 22 20",
+    "7 10 8 10 11",
+    "29 28 27 25 26 25 22 20",
+    "8 9 10 11"
 ]
 input_data = open("2-input.txt", "r")
 
 input = [[int(x) for x in line.split(" ")] for line in input_data]
 
 def is_safe_transition(report_is_ascending, current, next):
-	return current != next and \
-		(report_is_ascending and next > current and next <= current + 3) or \
-		(not report_is_ascending and next < current and next >= current - 3)
+    return current != next and \
+        (report_is_ascending and next > current and next <= current + 3) or \
+        (not report_is_ascending and next < current and next >= current - 3)
 
 def report_is_safe(report):
-	if len(report) < 2:
-		return True
-	
-	is_ascending = report[0] < report[1]
-	for (index, current) in enumerate(report[:-1]):
-		next = report[index + 1]
-		if not is_safe_transition(is_ascending, current, next):
-			# print("Unsafe datapoint transition from %d to %d (problem at index %d)" % (current, next, index), report)
-			return False
+    if len(report) < 2:
+        return True
+    
+    is_ascending = report[0] < report[1]
+    for (index, current) in enumerate(report[:-1]):
+        next = report[index + 1]
+        if not is_safe_transition(is_ascending, current, next):
+            # print("Unsafe datapoint transition from %d to %d (problem at index %d)" % (current, next, index), report)
+            return False
 
-	return True
+    return True
 
 # Part 1
 safe_report_count = 0
 for report in input:
-	if report_is_safe(report):
-		safe_report_count += 1
+    if report_is_safe(report):
+        safe_report_count += 1
 
 print("Number of safe reports: %d of %d total reports were safe" % (safe_report_count, len(input)))
 
 # Part 2
 safe_report_count = 0
 for report in input:
-	is_already_safe = report_is_safe(report)
-	if is_already_safe:
-		safe_report_count += 1
-		continue
+    is_already_safe = report_is_safe(report)
+    if is_already_safe:
+        safe_report_count += 1
+        continue
 
-	for index in range(len(report)):
-		snipped = report[:index] + report[index+1:]
-		is_safe = report_is_safe(snipped)
-		# print(snipped, is_safe)
-		if is_safe:
-			safe_report_count += 1
-			break
+    for index in range(len(report)):
+        snipped = report[:index] + report[index+1:]
+        is_safe = report_is_safe(snipped)
+        # print(snipped, is_safe)
+        if is_safe:
+            safe_report_count += 1
+            break
 
 print("Number of safe reports with the Problem Dampener: %d of %d total reports were safe" % (safe_report_count, len(input)))
--- 2024/3.py
+++ 2024/3.py
@@ -2,39 +2,39 @@ import re
 import functools
 
 input_data = [
-	"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 ]
 input_data = open("3-input.txt", "r")
 
 # Part 1
 total_sum = 0
 for line in input_data:
-	matches = re.findall("mul\(\d{1,3},\d{1,3}\)", line)
-	tokens = [re.split('\(|,|\)', x) for x in matches]
-	multiples = [int(x[1]) * int(x[2]) for x in tokens]
-	sum = functools.reduce(lambda x, y: x + y, multiples)
-	total_sum += sum
+    matches = re.findall("mul\(\d{1,3},\d{1,3}\)", line)
+    tokens = [re.split('\(|,|\)', x) for x in matches]
+    multiples = [int(x[1]) * int(x[2]) for x in tokens]
+    sum = functools.reduce(lambda x, y: x + y, multiples)
+    total_sum += sum
 
 print("Part 1: total sum is %d" % total_sum)
 
 # Part 2
 input_data = [
-	"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 ]
 input_data = open("3-input.txt", "r")
 
 do_mul = True
 total_sum = 0
 for line in input_data:
-	matches = re.findall("(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))", line)
-	for match in matches:
-		mul, dont, do = match
-		if do_mul and mul:
-			tokens = re.split('\(|,|\)', mul)
-			total_sum += int(tokens[1]) * int(tokens[2])
-		elif dont:
-			do_mul = False
-		elif do:
-			do_mul = True
+    matches = re.findall("(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))", line)
+    for match in matches:
+        mul, dont, do = match
+        if do_mul and mul:
+            tokens = re.split('\(|,|\)', mul)
+            total_sum += int(tokens[1]) * int(tokens[2])
+        elif dont:
+            do_mul = False
+        elif do:
+            do_mul = True
 
 print("Part 2: total sum is %d" % total_sum)
